e44998f6d052427332d2638c37193491874ba51a
sneko <supernekocat31@gmail.com>
2025-04-02 20:40:25 +0900

    feat: Implement array literals, access and manipulation functions

--- src/lexer.js
+++ src/lexer.js
@@ -9,8 +9,7 @@
  * @returns {string[]} 抽出されたトークンのリスト
  */
 function lexer(sourceCode) {
-    // "文字列" または print または 改行で分割
-    let tokens = sourceCode.split(/\/\/.*$|(\d+\.\d+|\d+|".*?"|\w+)|\s|(.)/m);
+    let tokens = sourceCode.split(/\/\/.*$|(\d+\.\d+|\d+|".*?"|\w+|\[|\]|\,)|\s|(.)/m);
     // splitの仕様上、undefinedや空文字列などが残るので、不要なものを除去
     return tokens.filter((token) => token);
 }
